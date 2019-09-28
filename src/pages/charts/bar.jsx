1ec3702e6552511cbc2f22a4d7116d3d0dd4e2b8
Aven <18438140598@163.com>
2019-09-28 21:06:09 +0800

    新增hooks版本图表用例

--- src/pages/charts/bar.jsx
+++ src/pages/charts/bar.jsx
@@ -1,10 +1,56 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { Card, Button } from 'antd'
+import ReactEcharts from 'echarts-for-react'
 
-export default class Bar extends Component {
+const Bar = () => {
 
-  render() {
-    return (
-      <div className="bar">Bar</div>
-    )
+  const [sales, setSales] = useState([5, 20, 36, 10, 10, 20])
+  const [inventories, setInventories] = useState([15, 30, 46, 20, 20, 40])
+
+  const updateCharts = (sales, inventories) => {
+    sales = sales.map(sale => sale + 1)
+    inventories = inventories.map(inventoriry => inventoriry - 1)
+    setSales(setSales)
+    setInventories(inventories)
   }
+  const getOption = () => {
+    return {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量', '库存']
+      },
+      xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: sales
+        },
+        {
+          name: '库存',
+          type: 'bar',
+          data: inventories
+        }
+      ]
+    }
+  }
+
+  return (
+    <div className="bar-container">
+      <Card>
+        <Button type="primary" onClick={() => updateCharts(sales, inventories)}>更新</Button>
+      </Card>
+      <Card title="柱状图">
+        <ReactEcharts option={getOption()} style={{height: 300}}/> 
+      </Card>
+    </div>
+  )
 }
+
+export default Bar
--- src/pages/charts/pie.jsx
+++ src/pages/charts/pie.jsx
@@ -1,10 +1,56 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Card } from 'antd'
+import ReactEcharts from 'echarts-for-react'
 
-export default class Pie extends Component {
-
-  render() {
-    return (
-      <div className="pie">Pie</div>
-    )
+const Pie = () => {
+  const getOption = () => {
+    return {
+      title : {
+        text: '某站点用户访问来源',
+        subtext: '纯属虚构',
+        x: 'center'
+      },
+      tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+      },
+      series : [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius : '55%',
+          center: ['50%', '60%'],
+          data: [
+            {value: 335, name: '直接访问'},
+            {value: 310, name: '邮件营销'},
+            {value: 234, name: '联盟广告'},
+            {value: 135, name: '视频广告'},
+            {value: 1548, name: '搜索引擎'}
+          ],
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    }
   }
+
+  return (
+    <div className="bar-container">
+      <Card title="饼图">
+        <ReactEcharts option={getOption()} style={{height: 500}}/> 
+      </Card>
+    </div>
+  )
 }
+
+export default Pie
